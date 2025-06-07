55b83a7e4567196cc224514a5b634d926a349841
Hamed Mahmoudkhani <ainyava@gmail.com>
2025-06-07 15:00:05 +0330

    feat(nvim): configure golang

--- .config/nvim/lua/yavavim/plugins/lang/go.lua
+++ .config/nvim/lua/yavavim/plugins/lang/go.lua
@@ -1,7 +1,15 @@
 return {
+  -- Synax highlight
   {
     "nvim-treesitter/nvim-treesitter",
     opts = { ensure_installed = { "go", "gomod", "gowork", "gosum" } },
+  },
+  -- lspconfig
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "gopls" },
+    },
   },
   {
     "neovim/nvim-lspconfig",
@@ -25,22 +33,51 @@ return {
       },
     },
   },
-  -- {
-  --   "leoluz/nvim-dap-go",
-  --   opts = {},
-  -- },
-  -- {
-  --   "fredrikaverpil/neotest-golang",
-  -- },
-  -- {
-  --   "echasnovski/mini.icons",
-  --   opts = {
-  --     file = {
-  --       [".go-version"] = { glyph = "", hl = "MiniIconsBlue" },
-  --     },
-  --     filetype = {
-  --       gotmpl = { glyph = "󰟓", hl = "MiniIconsGrey" },
-  --     },
-  --   },
-  -- },
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "goimports", "gofumpt" } },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "mason-org/mason.nvim",
+        opts = { ensure_installed = { "delve" } },
+      },
+      {
+        "leoluz/nvim-dap-go",
+        opts = {},
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    enabled = false,
+    dependencies = {
+      {"fredrikaverpil/neotest-golang"},
+      {
+        "leoluz/nvim-dap-go",
+        opts = {},
+      }
+    },
+    opts = {
+      adapters = {
+        ["neotest-golang"] = {
+          dap_go_enabled = true,
+        },
+      },
+    },
+  },
+  {
+    "echasnovski/mini.icons",
+    opts = {
+      file = {
+        [".go-version"] = { glyph = "", hl = "MiniIconsBlue" },
+      },
+      filetype = {
+        gotmpl = { glyph = "󰟓", hl = "MiniIconsGrey" },
+      },
+    },
+  },
 }
