a85ba6a6953a3e92f4a1102aa155811526161172
Hamed Mahmoudkhani <ainyava@gmail.com>
2025-06-08 18:51:26 +0330

    feat(nvim): added sql language

---
+++ .config/nvim/lua/yavavim/plugins/lang/sql.lua
@@ -0,0 +1,91 @@
+local sql_ft = { "sql", "mysql", "plsql" }
+
+return {
+  {
+    "mason-org/mason.nvim",
+    enabled = vim.g.use_mason,
+    opts = { ensure_installed = { "sqlfluff" } },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "sql" } },
+  },
+  {
+    "tpope/vim-dadbod",
+    cmd = "DB",
+  },
+  {
+    "kristijanhusak/vim-dadbod-completion",
+    dependencies = "vim-dadbod",
+    ft = sql_ft,
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = sql_ft,
+      })
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        default = { "dadbod" },
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
+      },
+    },
+    dependencies = {
+      "kristijanhusak/vim-dadbod-completion",
+    },
+  },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    dependencies = "vim-dadbod",
+    keys = {
+      { "<leader>D", "<cmd>DBUIToggle<CR>", desc = "Toggle DBUI" },
+    },
+    init = function()
+      local data_path = vim.fn.stdpath("data")
+  
+      vim.g.db_ui_auto_execute_table_helpers = 1
+      vim.g.db_ui_save_location = data_path .. "/dadbod_ui"
+      vim.g.db_ui_show_database_icon = true
+      vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
+      vim.g.db_ui_use_nerd_fonts = true
+      vim.g.db_ui_use_nvim_notify = true
+  
+      -- NOTE: The default behavior of auto-execution of queries on save is disabled
+      -- this is useful when you have a big query that you don't want to run every time
+      -- you save the file running those queries can crash neovim to run use the
+      -- default keymap: <leader>S
+      vim.g.db_ui_execute_on_save = false
+    end,
+  },
+  {
+    "kristijanhusak/vim-dadbod-completion",
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = function(_, opts)
+      for _, ft in ipairs(sql_ft) do
+        opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
+        table.insert(opts.linters_by_ft[ft], "sqlfluff")
+      end
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.formatters.sqlfluff = {
+        args = { "format", "--dialect=ansi", "-" },
+      }
+      for _, ft in ipairs(sql_ft) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        table.insert(opts.formatters_by_ft[ft], "sqlfluff")
+      end
+    end,
+  },
+}
