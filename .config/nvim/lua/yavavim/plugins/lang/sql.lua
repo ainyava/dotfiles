0148150a346f215be0fb15d8195b1f46b15529c5
Hamed Mahmoudkhani <ainyava@gmail.com>
2025-06-08 18:57:59 +0330

    feat(nvim): added yaml language

--- .config/nvim/lua/yavavim/plugins/lang/sql.lua
+++ .config/nvim/lua/yavavim/plugins/lang/sql.lua
@@ -64,6 +64,7 @@ return {
     "mfussenegger/nvim-lint",
     optional = true,
     opts = function(_, opts)
+      opts.linters_by_ft = opts.linters_by_ft or {}
       for _, ft in ipairs(sql_ft) do
         opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
         table.insert(opts.linters_by_ft[ft], "sqlfluff")
