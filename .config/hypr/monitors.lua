81a5ac17dca7e424c1e15a09a34e64072ab41f90
Hamed Mahmoudkhani <ainyava@gmail.com>
2026-09-13 13:16:55 +0330

    chore(hyprland): add monitor config

--- .config/hypr/monitors.lua
+++ .config/hypr/monitors.lua
@@ -5,10 +5,17 @@ local omarchy_gdk_scale = 1
 local omarchy_monitor_scale = 1
 
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy_monitor_scale })
 
--- Configure a specific monitor.
--- hl.monitor({ output = "DP-2", mode = "2560x1440@144", position = "0x0", scale = 1 })
+-- Monitors
+hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@60", position = "0x0", scale = omarchy_monitor_scale })
+hl.monitor({ output = "eDP-1", mode = "1920x1080@60", position = "1920x0", scale = omarchy_monitor_scale })
 
--- Portrait/rotated secondary monitor (transform: 1 = 90°, 3 = 270°).
--- hl.monitor({ output = "DP-2", mode = "preferred", position = "auto", scale = 1, transform = 1 })
+for i = 1, 9 do
+	hl.workspace_rule({
+		workspace = tostring(i),
+		monitor = "HDMI-A-1",
+		persistent = true,
+	})
+end
+
+hl.workspace_rule({ workspace = "10", monitor = "eDP-1", persistent = true })
